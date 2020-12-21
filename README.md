99ff7f6fc8b9dc17cb1e703ffeda19b3671a8e39
Vishal Pandey <vishal@gnani.ai>
2020-12-21 21:05:00 +0530

    docker added

--- README.md
+++
@@ -1,1 +0,0 @@
-# deploymentbot1
--- actions/actions.py
+++ actions/actions.py
@@ -5,23 +5,226 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+import json
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+knowledge = open("knowledgebase.json", encoding='utf-8')
+data = json.load(knowledge)
+f = {"morethan": 1, "lessthan": 0, "atleast": 0, "atmost": 0}
+k = ["morethan", "lessthan", 'atleast', 'atmost']
+
+
+class ActionNoodlesPrice(Action):
+    def name(self) -> Text:
+        return "action_show_noodles_price"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, any]]:
+        entities = tracker.latest_message['entities']
+        # print('entities ', entities)
+
+        if len(entities) == 0:
+            dispatcher.utter_message(template="utter_noodles_price")
+        else:
+            noodle_price = data['noodle_price']
+            # print(noodle_price)
+            if int(entities[0]['value']) == noodle_price:
+                dispatcher.utter_message(text="Yes")
+            else:
+                dispatcher.utter_message(text="No")
+
+        return []
+
+
+class ActionProfitMargin(Action):
+
+    def name(self) -> Text:
+        return "action_show_profit_margin"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities = tracker.latest_message['entities']
+        if len(entities) == 0:
+            dispatcher.utter_message(template="utter_profit_margin")
+        else:
+            margin = data['profit_margin']
+            if len(entities) == 1:
+                if int(entities[0]['value']) == margin:
+                    dispatcher.utter_message(text="Yes")
+                else:
+                    dispatcher.utter_message(text="No")
+            else:
+                pentities = [entities[0]['value'],entities[1]['value']]
+                g = 0  # mapped form f dictionary
+                for i in entities:
+                    print("i val ", i['value'])
+                    if i['value'] in k:
+                        g = f[i['value']]
+                        pentities.remove(i['value'])
+                        break
+                if int(pentities[0]) + g == margin:
+                    dispatcher.utter_message(text="Yes")
+                else:
+                    dispatcher.utter_message(text="No")
+
+
+        return []
+
+
+class ActionNumberOfCustomer(Action):
+
+    def name(self) -> Text:
+        return "action_show_number_of_customer"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities = tracker.latest_message['entities']
+        if len(entities) == 0:
+            dispatcher.utter_message(template="utter_number_of_customer")
+        else:
+            customer = data['number_customer']
+            if len(entities) == 1:
+                if int(entities[0]['value']) <= customer:
+                    dispatcher.utter_message(text="Yes")
+                else:
+                    dispatcher.utter_message(text="No")
+            else:
+                pentities = [entities[0]['value'],entities[1]['value']]
+                g = 0  # mapped form f dictionary
+                for i in entities:
+                    print("i val ", i['value'])
+                    if i['value'] in k:
+                        g = f[i['value']]
+                        pentities.remove(i['value'])
+                        break
+                if int(pentities[0]) + g <= customer:
+                    dispatcher.utter_message(text="Yes")
+                else:
+                    dispatcher.utter_message(text="No")
+
+        return []
+
+
+class ActionPaymentMode(Action):
+
+    def name(self) -> Text:
+        return "action_show_mode_of_payment"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities = tracker.latest_message['entities']
+        if len(entities) == 0:
+            dispatcher.utter_message(template="utter_mode_of_payment")
+        else:
+            payment_mode = data['mode']
+            if entities[0]['value'] == payment_mode:
+                dispatcher.utter_message(text="Yes")
+            else:
+                dispatcher.utter_message(text="No")
+
+        return []
+
+
+class ActionCashDineTakeaway(Action):
+
+    def name(self) -> Text:
+        return "action_show_cash_dine_takeaway"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities = tracker.latest_message['entities']
+        if len(entities) == 1:
+            if entities[0]['value'] == "dine in":
+                dispatcher.utter_message(template="utter_cash_dine")
+            else:
+                dispatcher.utter_message(template="utter_cash_takeaway")
+        else:
+            dine = data['cash_dine_in']
+            takeaway = data['cash_takeaway']
+            a = 'dine in'
+            b = 'takeaway'
+            if len(entities)==3:
+                c = [entities[0]['value'], entities[1]['value'], entities[2]['value']]
+            else:
+                c = [entities[0]['value'], entities[1]['value']]
+            g = 0  # mapped form f dictionary
+            for i in entities:
+                print("i val ", i['value'])
+                if i['value'] in k:
+                    g = f[i['value']]
+                    c.remove(i['value'])
+                    break
+            if a in c:
+                d = 1 - c.index(a)
+                e = int(c[d]) + g
+                if e <= dine:
+                    dispatcher.utter_message(text="Yes")
+                else:
+                    dispatcher.utter_message(text="No")
+            else:
+                d = 1 - c.index(b)
+                e = int(c[d])
+                if e <= takeaway:
+                    dispatcher.utter_message(text="Yes")
+                else:
+                    dispatcher.utter_message(text="No")
+
+        return []
+
+
+class ActionCreditDineTakeaway(Action):
+
+    def name(self) -> Text:
+        return "action_show_credit_card_dine_takeaway"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities = tracker.latest_message['entities']
+        print("entities ",entities)
+        if len(entities) == 1:
+            if entities[0]['value'] == "dine in":
+                dispatcher.utter_message(template="utter_credit_card_dine")
+            else:
+                dispatcher.utter_message(template="utter_credit_card_takeaway")
+        else:
+            dine = data['credit_dine_in']
+            takeaway = data['credit_takeaway']
+            a = 'dine in'
+            b = 'takeaway'
+            if len(entities)==3:
+                c = [entities[0]['value'], entities[1]['value'], entities[2]['value']]
+            else:
+                c = [entities[0]['value'], entities[1]['value']]
+            g = 0 #mapped form f dictionary
+            for i in entities:
+                print("i val ", i['value'])
+                if i['value'] in k:
+                    g = f[i['value']]
+                    c.remove(i['value'])
+                    break
+            if a in c:
+                d = 1 - c.index(a)
+                e = int(c[d]) + g
+                if e <= dine:
+                    dispatcher.utter_message(text="Yes")
+                else:
+                    dispatcher.utter_message(text="No")
+            else:
+                d = 1 - c.index(b)
+                e = int(c[d])
+                if e <= takeaway:
+                    dispatcher.utter_message(text="Yes")
+                else:
+                    dispatcher.utter_message(text="No")
+
+        return []
